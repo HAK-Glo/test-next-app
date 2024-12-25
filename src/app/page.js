1734879c917e4e2108127446e4c3bcc6e6df3fd0
hak <hak.globaliasoft@gmail.com>
2024-12-25 16:34:36 +0530

    added google analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <Script
+        id="next"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-1XC4SRFQ7Y`}
+      ></Script>
+      <Script id="next">
+        {`window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-1XC4SRFQ7Y');`}
+      </Script>
+      <main className={styles.main}>
+        <h2>Hello, This is nextjs app with Google analytics.</h2>
+      </main>
+    </>
+  );
 }
